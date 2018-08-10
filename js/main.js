5dacba038277b1e1d3bf53a5cbca772b5ae99a50
Bradley Murchison <bradleymurchison@gmail.com>
2018-08-10 18:46:17 -0500

    fetch update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 const employeeDivs = document.querySelectorAll('main div');
 let employeesArr = [];
 
+fetchData();
+
 // -----------------------------------------
 // FETCH FUNCTIONS
 // -----------------------------------------
 
 function fetchData() { return fetch('https://randomuser.me/api/?results=12&nat=nz&inc=name,email,location,picture')
     .then(response => response.json())
-    .then(data => employeesArr = data.results)
-};
+    .then(data => {
 
-fetchData();
-generateEmployees(employeesArr);
+        employeesArr = data.results
+    })
+    .then(generateEmployees(employeesArr))
+};
 
 // -----------------------------------------
 // HELPER FUNCTIONS
@@ -22,14 +25,15 @@ function generateEmployees(arr) {
     for (let i = 0; i < arr.length; i++) {
         let employee = arr[i];
         let html = `
-                    <img src="${employee.picture.thumbnail.value}" alt="faceshot of ">
-                    <h2 id="name">${employee.name.first.value} ${employee.name.last.value}</h2>
-                    <p id="email">${employee.email.value}</p><br>
-                    <p id="location">${employee.location.city.value}</p>
+                    <img src="${employee.picture.thumbnail}" alt="faceshot of ">
+                    <h2 id="name">${employee.name.first} ${employee.name.last}</h2>
+                    <p id="email">${employee.email}</p><br>
+                    <p id="location">${employee.location.city}</p>
                     `;
 
         employeeDivs[i].setAttribute("class", `employee__${i}`)
         employeeDivs[i].innerHTML(html);
 
     };
-}
+};
+
