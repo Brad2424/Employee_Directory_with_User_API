4969d5374def4da4575e20962ddc1767f8c765b6
Bradley Murchison <bradleymurchison@gmail.com>
2018-08-12 16:57:16 -0500

    JS for modal window

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 const employeeDivs = document.querySelectorAll('div.employee');
-const modalWindowLinks = document.querySelectorAll('.modalWindow');
 const modalContentDivs = document.querySelectorAll('.modalContent');
+const modalWindows = document.querySelectorAll('.modalWindow');
 let employeesArr = [];
+
+
+const modalWindowLinks = document.querySelectorAll('.modalLink');
+const closeSpan = document.getElementsByClassName("close"); 
+
+// When the user clicks on a modal link, open the corresponding modal window
+for (let i = 0; i < modalWindowLinks.length; i++) {
+    const modalWindowLink = modalWindowLinks[i];
+    const modalWindow = modalWindows[i];
+
+    modalWindowLink.onclick = function () {
+        modalWindow.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    closeSpan.onclick = function() {
+        modalWindow.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modalWindow[i]) {
+            modalWindow.style.display = "none";
+        }
+    }
+}
+
 
 fetchAndAddToPage();
 
@@ -14,9 +41,7 @@ function fetchAndAddToPage() { return fetch('https://randomuser.me/api/?results=
     .then(data => {
         employeesArr = data.results
         addToPage(employeesArr)
-        // addToModalWindow(employeesArr)
     })
-    // .then(addToModalWindow(employeesArr))
 };
 
 // -----------------------------------------
@@ -28,6 +53,22 @@ function addToPage(arr) {
     for (let i = 0; i < arr.length; i++) {
         const employee = arr[i];
         const html = `
+                    <div class ="modalWindow" >
+                        <div class="modalContent">
+                            <span class="close">&times;</span>
+                            <img src="${employee.picture.large}" alt="faceshot of ${employee.name.first} ${employee.name.last}">
+                            <div class="personalInfo">
+                                <h2 id="name">${employee.name.first} ${employee.name.last}</h2><br>
+                                <a id="email">${employee.email}</a><br>
+                                <p id="location">${employee.location.city}</p>
+                            </div>
+                            <div class="personalInfo">
+                                <p id="cell">${employee.cell}</p>
+                                <p id="address">${employee.location.street}, ${employee.location.state} ${employee.location.postcode}</p>
+                                <p id="dob">${employee.dob.date}</p>
+                            </div>
+                        </div>
+                    </div>
                     <img src="${employee.picture.medium}" alt="faceshot of ${employee.name.first} ${employee.name.last}">
                     <div class="personalInfo">
                         <h2 id="name">${employee.name.first} ${employee.name.last}</h2><br>
@@ -40,28 +81,3 @@ function addToPage(arr) {
         employeeDivs[i].innerHTML += html;
     }
 }
-
-function addToModalWindow(arr) {
-
-    for (let i = 0; i < arr.length; i++) {
-        const employee = arr[i];
-        const html = `
-                    <span class="close">&times;</span>
-                    <img src="${employee.picture.large}" alt="faceshot of ${employee.name.first} ${employee.name.last}">
-                    <div class="personalInfo">
-                        <h2 id="name">${employee.name.first} ${employee.name.last}</h2><br>
-                        <a id="email">${employee.email}</a><br>
-                        <p id="location">${employee.location.city}</p>
-                    </div>
-                    <div class="personalInfo">
-                        <p id="cell">${employee.cell}</p>
-                        <p id="address">${employee.location.street}, ${employee.location.state} ${employee.location.postcode}</p>
-                        <p id="dob">${employee.dob.date}</p>
-                    </div>                    
-                    `;
-
-        modalContentDivs[i].classList.add(`employee__${i}`);
-        modalContentDivs[i].innerHTML = html;
-    }
-}
-
