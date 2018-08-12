0266440a2994b0f4a00e755b79ef0b2ca62bc009
Bradley Murchison <bradleymurchison@gmail.com>
2018-08-12 15:43:56 -0500

    Modal window added

--- js/main.js
+++ js/main.js
@@ -1,39 +1,67 @@
-const employeeDivs = document.querySelectorAll('main div');
+const employeeDivs = document.querySelectorAll('div.employee');
+const modalWindowLinks = document.querySelectorAll('.modalWindow');
+const modalContentDivs = document.querySelectorAll('.modalContent');
 let employeesArr = [];
 
-fetchData();
+fetchAndAddToPage();
 
 // -----------------------------------------
 // FETCH FUNCTIONS
 // -----------------------------------------
 
-function fetchData() { return fetch('https://randomuser.me/api/?results=12&nat=nz&inc=name,email,location,picture')
+function fetchAndAddToPage() { return fetch('https://randomuser.me/api/?results=12&nat=nz&inc=name,email,location,picture,cell,dob')
     .then(response => response.json())
     .then(data => {
-
         employeesArr = data.results
+        addToPage(employeesArr)
+        // addToModalWindow(employeesArr)
     })
-    .then(generateEmployees(employeesArr))
+    // .then(addToModalWindow(employeesArr))
 };
 
 // -----------------------------------------
 // HELPER FUNCTIONS
 // -----------------------------------------
 
-function generateEmployees(arr) {
+function addToPage(arr) {
 
     for (let i = 0; i < arr.length; i++) {
-        let employee = arr[i];
-        let html = `
-                    <img src="${employee.picture.thumbnail}" alt="faceshot of ">
-                    <h2 id="name">${employee.name.first} ${employee.name.last}</h2>
-                    <p id="email">${employee.email}</p><br>
-                    <p id="location">${employee.location.city}</p>
+        const employee = arr[i];
+        const html = `
+                    <img src="${employee.picture.medium}" alt="faceshot of ${employee.name.first} ${employee.name.last}">
+                    <div class="personalInfo">
+                        <h2 id="name">${employee.name.first} ${employee.name.last}</h2><br>
+                        <a id="email">${employee.email}</a><br>
+                        <p id="location">${employee.location.city}</p>
+                    </div>
                     `;
 
-        employeeDivs[i].setAttribute("class", `employee__${i}`)
-        employeeDivs[i].innerHTML(html);
+        employeeDivs[i].setAttribute("class", `employee__${i}`);
+        employeeDivs[i].innerHTML += html;
+    }
+}
 
-    };
-};
+function addToModalWindow(arr) {
+
+    for (let i = 0; i < arr.length; i++) {
+        const employee = arr[i];
+        const html = `
+                    <span class="close">&times;</span>
+                    <img src="${employee.picture.large}" alt="faceshot of ${employee.name.first} ${employee.name.last}">
+                    <div class="personalInfo">
+                        <h2 id="name">${employee.name.first} ${employee.name.last}</h2><br>
+                        <a id="email">${employee.email}</a><br>
+                        <p id="location">${employee.location.city}</p>
+                    </div>
+                    <div class="personalInfo">
+                        <p id="cell">${employee.cell}</p>
+                        <p id="address">${employee.location.street}, ${employee.location.state} ${employee.location.postcode}</p>
+                        <p id="dob">${employee.dob.date}</p>
+                    </div>                    
+                    `;
+
+        modalContentDivs[i].classList.add(`employee__${i}`);
+        modalContentDivs[i].innerHTML = html;
+    }
+}
 
