46ecc510352978812c64118435ba11392e76b34d
Bradley Murchison <bradleymurchison@gmail.com>
2018-11-04 17:23:23 -0500

    changed to async/await syntax

--- js/main.js
+++ js/main.js
@@ -5,66 +5,65 @@ let amployeeNames = [];
 let employeeLocations = [];
 
 // -----------------------------------------
-// FETCH FUNCTION
+// FETCH DATA FROM API AND RENDER TO PAGE
 // -----------------------------------------
 
-function fetchAndAddToPage() { return fetch('https://randomuser.me/api/?results=12&nat=nz&inc=name,email,location,picture,cell,dob')
-    .then(response => response.json())
-    .then(data => {
-        employeesArr = data.results
-        addToPage(employeesArr);
-        addEventListners();
-    })
-};
+(async () => { 
+    let response = await fetch('https://randomuser.me/api/?results=12&nat=nz&inc=name,email,location,picture,cell,dob');
+    let data = await response.json();
+    let employeesArr = data.results;
 
-fetchAndAddToPage();
+    renderEmployee(employeesArr);
+    modalButtons();
+})();
 
 // -----------------------------------------
 // HELPER FUNCTIONS
 // -----------------------------------------
 
-function addToPage(arr) {
+function renderEmployee(arr) {
 
     for (let i = 0; i < arr.length; i++) {
         const employee = arr[i];
         const html = `
-                    <div class ="modalWindow" >
-                        <span class="close">&times;</span>
+            <div class ="modalWindow" >
+                <span class="close">&times;</span>
 
-                        <div class="modalContent">
-                            <span class="left">&#8678;</span>
-                            <span class="right">&#8680;</span>
-                            <img src="${employee.picture.large}" alt="faceshot of ${employee.name.first} ${employee.name.last}">
-                            <div class="personalInfo">
-                                <h2 id="name">${employee.name.first} ${employee.name.last}</h2>
-                                <a id="email">${employee.email}</a>
-                                <p id="location">${employee.location.city}</p>
-                            </div>
-                            <div class="personalInfo extraInfo">
-                                <p id="cell">${employee.cell}</p>
-                                <p id="address">${employee.location.street}, ${employee.location.state} ${employee.location.postcode}</p>
-                                <p id="dob">Birthday: ${employee.dob.date.substring(5,7)}/${employee.dob.date.substring(8,10)}/${employee.dob.date.substring(0,4)}</p>
-                            </div>
-                        </div>
+                <div class="modalContent">
+                    <span class="left">&#8678;</span>
+                    <span class="right">&#8680;</span>
+                    <img src="${employee.picture.large}" alt="faceshot of ${employee.name.first} ${employee.name.last}">
+                    <div class="personalInfo">
+                        <h2 id="name">${employee.name.first} ${employee.name.last}</h2>
+                        <a id="email">${employee.email}</a>
+                        <p id="location">${employee.location.city}</p>
                     </div>
-                    <div class = card>
-                        <img src="${employee.picture.medium}" alt="faceshot of ${employee.name.first} ${employee.name.last}">
-                        <div class="personalInfo">
-                            <h2 id="name">${employee.name.first} ${employee.name.last}</h2>
-                            <a id="email">${employee.email}</a>
-                            <p id="location">${employee.location.city}</p>
-                        </div>
+                    <div class="personalInfo extraInfo">
+                        <p id="cell">${employee.cell}</p>
+                        <p id="address">${employee.location.street}, ${employee.location.state} ${employee.location.postcode}</p>
+                        <p id="dob">Birthday: ${employee.dob.date.substring(5,7)}/${employee.dob.date.substring(8,10)}/${employee.dob.date.substring(0,4)}</p>
                     </div>
-                    `;
+                </div>
+            </div>
+            <div class = card>
+                <img src="${employee.picture.medium}" alt="faceshot of ${employee.name.first} ${employee.name.last}">
+                <div class="personalInfo">
+                    <h2 id="name">${employee.name.first} ${employee.name.last}</h2>
+                    <a id="email">${employee.email}</a>
+                    <p id="location">${employee.location.city}</p>
+                </div>
+            </div>
+            `;
 
         employeeDivs[i].setAttribute("class", `employee__${i}`);
         employeeDivs[i].setAttribute("data-search", `${employee.name.first}${employee.name.last} ${employee.location.city}`);
+
         employeeDivs[i].innerHTML += html;
     }
 }
 
 
-function addEventListners() {
+function modalButtons() {
     const closeSpans = document.getElementsByClassName('close');
     const leftSpans = document.getElementsByClassName('left');
     const rightSpans = document.getElementsByClassName('right');
@@ -102,6 +101,10 @@ function addEventListners() {
     }
 }
 
+// -----------------------------------------
+// SEARCH BOX FUNCTIONALITY
+// -----------------------------------------
+
 function searchBox() {
     const input = document.getElementById("searchInput");
     const filter = input.value.toUpperCase();
@@ -114,3 +117,4 @@ function searchBox() {
         }
     }
 }
+
